fd0702c0baf5ed125b908c990e6620e49d982708
Naveen M K <naveen@syrusdark.website>
2020-12-29 08:39:37 +0530

    Build Pango for using with Wheels (#4)
    * Build pango while building wheels
    provides latest version of pango in the build which seems
    to working on docker images provided by python but still not sure
    whether things will work for all
    if things didn't work for anybody they can still build from source.
    
    * ci: test for wheels after they are build for linux
    
    * Update LICENSE for binary files
    
    * lint
    
    * fix windows tests
    windll -> packing

---
+++ packing/download_and_extract.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import argparse
+import logging
+import shutil
+import tarfile
+import tempfile
+from pathlib import Path
+
+import requests
+
+logging.basicConfig(format="%(levelname)s - %(message)s", level=logging.INFO)
+parser = argparse.ArgumentParser(description="Download things.")
+parser.add_argument("url", help="the url to download")
+parser.add_argument("folder", help="folder name to extract")
+args = parser.parse_args()
+
+a = requests.get(args.url)
+logging.info("Download Complete Extracting")
+assert a.status_code != 404, "Does not exist."
+with tempfile.TemporaryDirectory() as tmpdirname:
+    tmpdirname = Path(tmpdirname)
+    fname = Path(args.url).name
+    for i in tmpdirname.iterdir():
+        print(i)
+    with open(tmpdirname / fname, "wb") as f:
+        logging.info(f"Saving to {tmpdirname / fname}")
+        f.write(a.content)
+    with tarfile.open(tmpdirname / fname, "r") as tar:
+        logging.info(f"Extracting {tmpdirname / fname} to {tmpdirname}")
+        tar.extractall(tmpdirname)
+    logging.info(
+        f"Moving {str(tmpdirname / Path(Path(args.url).stem).stem)}",
+        f"to {str(args.folder)}",
+    )
+    shutil.move(
+        str(tmpdirname / Path(Path(args.url).stem).stem),
+        str(args.folder),
+    )
