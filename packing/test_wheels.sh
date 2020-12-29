0b01e296037ebfd607da5899d6b809130a5b7704
Syrus Dark <naveen@syrusdark.website>
2020-12-29 19:31:29 +0530

    ci: fix wheel build test

---
+++ packing/test_wheels.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#test wheels in different directory
+set -e
+package=$1
+
+FILE_PATH="`dirname \"$0\"`"
+FILE_PATH="`( cd \"$FILE_PATH\" && pwd )`"
+if [ -z "$FILE_PATH" ] ; then
+  exit 1
+fi
+
+cd $TMP
+cp -r $package/tests mtests
+pytest -s mtests
+rm -r mtests
+cd $FILE_PATH
