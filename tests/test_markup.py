5f793d9c18df5f79fd7e8a0deb92d18c08473d3c
Philipp Imhof <52650214+PhilippImhof@users.noreply.github.com>
2021-01-10 21:09:34 +0100

    Better error messages if tests fail

--- tests/test_markup.py
+++ tests/test_markup.py
@@ -2,12 +2,18 @@
 def test_good_markup():
     import manimpango  # noqa: F401
 
-    assert manimpango.MarkupUtils.validate("foo")
-    assert manimpango.MarkupUtils.validate("<b>bar</b>")
+    assert manimpango.MarkupUtils.validate("foo"), '"foo" should not fail validation'
+    assert manimpango.MarkupUtils.validate(
+        "<b>bar</b>"
+    ), '"<b>bar</b>" should not fail validation'
 
 
 def test_bad_markup():
     import manimpango  # noqa: F401
 
-    assert not manimpango.MarkupUtils.validate("<b>foo")
-    assert not manimpango.MarkupUtils.validate("<xyz>foo</xyz>")
+    assert not manimpango.MarkupUtils.validate(
+        "<b>foo"
+    ), '"<b>foo" should fail validation (unbalanced tags)'
+    assert not manimpango.MarkupUtils.validate(
+        "<xyz>foo</xyz>"
+    ), '"<xyz>foo</xyz>" should fail validation (invalid tag)'
