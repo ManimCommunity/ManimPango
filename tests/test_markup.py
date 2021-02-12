e6dbd91577430575189a5600c995f89d96bba72b
Syrus Dark <naveen@syrusdark.website>
2021-02-12 13:08:03 +0530

    Add unicode support to MarkupUtils.validata It was broken in previous commits

--- tests/test_markup.py
+++ tests/test_markup.py
@@ -8,6 +8,9 @@ def test_good_markup():
     assert manimpango.MarkupUtils.validate(
         "<b>bar</b>"
     ), '"<b>bar</b>" should not fail validation'
+    assert manimpango.MarkUtils.validate(
+        "வணக்கம்",
+    ), '"வணக்கம்" should not fails'
 
 
 def test_bad_markup():
