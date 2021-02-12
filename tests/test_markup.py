7e0ba996b914647c964cf6879ee28664e82d1c8b
Syrus Dark <naveen@syrusdark.website>
2021-02-12 13:14:24 +0530

    fix typo

--- tests/test_markup.py
+++ tests/test_markup.py
@@ -8,7 +8,7 @@ def test_good_markup():
     assert manimpango.MarkupUtils.validate(
         "<b>bar</b>"
     ), '"<b>bar</b>" should not fail validation'
-    assert manimpango.MarkUtils.validate(
+    assert manimpango.MarkupUtils.validate(
         "வணக்கம்",
     ), '"வணக்கம்" should not fails'
 
