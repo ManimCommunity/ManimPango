63d4ec4a9aa2bfcef53cf2456b9991ac56c63dfb
Ricky Chon <rickychon99@gmail.com>
2021-04-04 02:01:36 -0400

    Add detailed message for ImportError in __init__ (#50)
    * Add detailed message for ImportError in __init__
    
    * Slap GitHub Actions
    
    * Update import error message to be more verbose
    
    * Update manimpango/__init__.py
    
    Co-authored-by: Naveen M K <naveen@syrusdark.website>
    
    * Update manimpango/__init__.py
    
    Co-authored-by: Naveen M K <naveen@syrusdark.website>
    
    * Update manimpango/__init__.py
    
    * Raise error with custom message
    
    Co-authored-by: Naveen M K <naveen@syrusdark.website>

--- manimpango/__init__.py
+++ manimpango/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import sys
 
 from ._version import __version__  # noqa: F403,F401
 
@@ -13,5 +14,22 @@ try:
     from .cmanimpango import *  # noqa: F403,F401
     from .enums import *  # noqa: F403,F401
     from .register_font import *  # noqa: F403,F401
-except ImportError:  # pragma: no cover
-    raise ImportError("Couldn't load the necessary Shared Libraries.")
+except ImportError as ie:  # pragma: no cover
+    py_ver = ".".join(map(str, sys.version_info[:3]))
+    msg = f"""
+
+ManimPango could not import and load the necessary shared libraries.
+This error may occur when ManimPango and its dependencies are improperly set up.
+Please make sure the following versions are what you expect:
+
+    * ManimPango v{__version__}, Python v{py_ver}
+
+If you believe there is a greater problem,
+feel free to contact us or create an issue on GitHub:
+
+    * Discord: https://discord.gg/mMRrZQW
+    * GitHub: https://github.com/ManimCommunity/ManimPango/issues
+
+Original error: {ie}
+"""
+    raise ImportError(msg)
