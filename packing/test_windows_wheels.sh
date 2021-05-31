9fca7dfcf64655edf2b64653c70515a52749ca4b
Naveen M K <naveen@syrusdark.website>
2021-05-31 16:12:57 +0530

    Try fixing

--- packing/test_windows_wheels.sh
+++ packing/test_windows_wheels.sh
@@ -5,11 +5,21 @@ set -x
 
 PYTHON_VERSION=$(python -c "import sys;print(f'{sys.version_info.major}{sys.version_info.minor}')")
 BITNESS=$1
+PACKAGE=$2
 
 if [[ "$PYTHON_VERSION" == "36" || "$BITNESS" == "32" ]]; then
     # For Python 3.6 and 32-bit architecture use the regular
     # test command (outside of the minimal Docker container)
-    pytest
+    FILE_PATH="`dirname \"$0\"`"
+    FILE_PATH="`( cd \"$FILE_PATH\" && pwd )`"
+    if [ -z "$FILE_PATH" ] ; then
+        exit 1
+    fi
+    cd $TMP
+    cp -r $package/tests mtests
+    pytest -s mtests
+    rm -r mtests
+    cd $FILE_PATH
 else
     docker container run --rm ManimCommunity/minimal-windows \
                          powershell -Command "pytest"
