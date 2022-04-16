6f74c5a0f37fa816eb2c098afd23b88ac63f043c
Naveen M K <naveen521kk@gmail.com>
2022-04-16 13:05:57 +0530

    Add tests for font_desc

--- tests/test_font_desc.py
+++ tests/test_font_desc.py
@@ -2,14 +2,45 @@
 import copy
 
 from manimpango.fonts import FontDescription
+from manimpango.fonts.enums import Style, Variant, Weight
 
 
-def test_init():
-    _a = FontDescription()
-    assert repr(_a) == "Normal"
+class TestFontDescription:
+    def test_init(self):
+        _a = FontDescription()
+        assert _a.family is None
+        assert repr(_a) == "Normal"
+        _a = FontDescription(
+            family="hello",
+            size=30,
+            style=Style.OBLIQUE,
+            weight=Weight.BOOK,
+            variant=Variant.NORMAL,
+        )
+        assert _a.family == "hello"
+        assert _a.size == 30
+        assert _a.style == Style.OBLIQUE
+        assert _a.weight == Weight.BOOK
+        assert _a.variant == Variant.NORMAL
 
+    def test_copy(self):
+        _a = FontDescription(family="nice", size=90)
+        _b = copy.copy(_a)
+        assert _a == _b
+        _c = copy.deepcopy(_a)
+        assert _a == _c
 
-def test_copy():
-    _a = FontDescription()
-    _b = copy.copy(_a)
-    assert _a == _b
+    def test_from_string(self):
+        _a = FontDescription.from_string("OhNO 12")
+        assert _a.family == "OhNO"
+        assert _a.size == 12288  # 12 -> pango units
+        _a = FontDescription.from_string("Cantarell Italic Light 15")
+        assert _a.family == "Cantarell"
+        assert _a.size == 15360
+        assert _a.style == Style.ITALIC
+        assert _a.weight == Weight.LIGHT
+        assert _a.variant == Variant.NORMAL
+
+    def test_str_same_as_repr(self):
+        _a = FontDescription.from_string("Cantarell Italic Light 15")
+        assert repr(_a) == str(_a)
