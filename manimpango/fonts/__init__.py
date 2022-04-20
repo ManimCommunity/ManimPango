f665b30bbc6c11c5f8cff07ee31bc54115e946e0
Naveen M K <naveen521kk@gmail.com>
2022-04-20 21:54:44 +0530

    Slight format

--- manimpango/fonts/__init__.py
+++ manimpango/fonts/__init__.py
@@ -5,4 +5,4 @@ and other enums related to it.
 
 __all__ = ["FontDescription", "Style", "Weight", "Variant"]
 from ._font_desc import FontDescription
-from .enums import Style, Weight, Variant
+from .enums import Style, Variant, Weight
