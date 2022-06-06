8a72174ab28c94d73a6afa813ec864d3aac7ba38
Naveen M K <naveen521kk@gmail.com>
2022-06-06 14:59:26 +0000

    Implement a `render()` which will render into an SVG

--- manimpango/renderer/svg_renderer.pxd
+++ manimpango/renderer/svg_renderer.pxd
@@ -3,15 +3,19 @@ from pango cimport *
 from glib cimport *
 
 from ..layout import Layout
+from ..fonts import FontDescription
 
 cdef class SVGRenderer:
     cdef cairo_surface_t* cairo_surface
     cdef cairo_t* cairo_context
     cdef PangoLayout* pango_layout
+    cdef PangoFontDescription* pango_font_desc
 
     cdef str file_name
     cdef float width
     cdef float height
     cdef object py_layout
+    cdef object py_font_desc
 
     cdef start_renderering(self)
+    cpdef render(self)
--- manimpango/renderer/svg_renderer.pyx
+++ manimpango/renderer/svg_renderer.pyx
@@ -21,30 +21,51 @@ cdef class SVGRenderer:
         self.cairo_surface = surface
         self.cairo_context = create_cairo_context_from_surface(surface)
         self.pango_layout = create_pango_layout(self.cairo_context)
+        self.pango_font_desc = create_font_desc()
 
     def __init__(
         self,
         file_name: str,
         width: float,
         height: float,
-        layout: Layout,
-        font_desc: FontDescription
+        layout: Layout
     ):
         self.file_name = file_name
         self.width = width
         self.height = height
         self.py_layout = layout
+        self.py_font_desc = layout.font_desc
 
     cdef start_renderering(self):
         pylayout_to_pango_layout(self.pango_layout, self.py_layout)
+        pyfontdesc_to_pango_font_desc(self.pango_font_desc, self.py_font_desc)
         pango_layout_set_font_description(
             self.pango_layout,
-            const PangoFontDescription* desc
+            self.pango_font_desc
         )
+        # Check if the context is fine
+        _err = is_context_fine(self.cairo_context)
+        if _err != "":
+            raise Exception(_err)
+        
+        # Assign the font description to the layout
+        pango_layout_set_font_description(self.pango_layout,
+            self.pango_font_desc)
 
+        # Render the actual layout into the cairo context
+        pango_cairo_show_layout(self.cairo_context,
+            self.pango_layout)
+    
+        # Check if the context is fine again
+        _err = is_context_fine(self.cairo_context)
+        if _err != "":
+            raise Exception(_err)
+
+    cpdef render(self):
+        self.start_renderering()
 
     def __dealloc__(self):
+        g_object_unref(self.pango_layout)
         cairo_destroy(self.cairo_context)
         cairo_surface_destroy(self.cairo_surface)
-        g_object_unref(self.pango_layout)
-
+        pango_font_description_free(self.pango_font_desc)
