32a5fc2ac4888b20d28e3886df4958eef62f9094
Naveen M K <naveen521kk@gmail.com>
2022-07-26 11:33:23 +0000

    Format files

--- manimpango/exceptions.py
+++ manimpango/exceptions.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 class MarkupParseError(Exception):
-    """MarkupParseError is raised when the markup passed in invalid.
-    """
+    """MarkupParseError is raised when the markup passed in invalid."""
+
     pass
