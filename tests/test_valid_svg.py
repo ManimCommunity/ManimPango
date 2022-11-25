c5d02df6b143e608766786e6b5f04fe18218aed4
Naveen M K <naveen521kk@gmail.com>
2022-11-25 22:56:17 +0530

    tests: check whether the SVG files are valid
    let's see if we can reproduce the error

---
+++ tests/test_valid_svg.py
@@ -0,0 +1,13 @@
+from ._manim import Text
+from pathlib import Path
+from xml.parsers.expat import ParserCreate
+
+def test_whether_valid_svg_file(tmpdir):
+    loc = Path(tmpdir, "test.svg")
+    assert not loc.exists()
+    Text("{ }", color="red", filename=str(loc))
+    assert loc.exists()
+    # verify that it's a valid svg file by parsing it
+    with open(loc, "rb") as f:
+        p = ParserCreate()
+        p.ParseFile(f)
