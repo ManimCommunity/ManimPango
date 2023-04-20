0942e738726e9c46fbd511756241b05765452556
Naveen M K <naveen521kk@gmail.com>
2023-04-20 19:24:24 +0530

    format

--- packing/_distributor_init_win.py
+++ packing/_distributor_init_win.py
@@ -1,36 +1,35 @@
-
-'''
+"""
 Helper to preload windows dlls to prevent dll not found errors.
 Once a DLL is preloaded, its namespace is made available to any
 subsequent DLL. This file is created as part of the scripts that
 build the wheel.
-'''
-import os
+"""
 import glob
-if os.name == 'nt':
+import os
+
+if os.name == "nt":
     # convention for storing / loading the DLL from
     # ManimPango/.libs/, if present
     basedir = os.path.dirname(__file__)
-    libs_dir = os.path.abspath(os.path.join(basedir, '.libs'))
+    libs_dir = os.path.abspath(os.path.join(basedir, ".libs"))
 
     # first add them to os.add_dll_directory
     try:
-        if hasattr(os, 'add_dll_directory'):
-            libs_dir = os.path.abspath(os.path.join(basedir, '.libs'))
+        if hasattr(os, "add_dll_directory"):
+            libs_dir = os.path.abspath(os.path.join(basedir, ".libs"))
             if os.path.isdir(libs_dir):
                 os.add_dll_directory(libs_dir)
-    except:
+    except Exception:
         pass
 
     try:
         from ctypes import WinDLL
-    except:
+    except Exception:
         pass
     else:
         DLL_filenames = []
         if os.path.isdir(libs_dir):
-            for filename in glob.glob(os.path.join(libs_dir,
-                                                   '*openblas*dll')):
+            for filename in glob.glob(os.path.join(libs_dir, "*openblas*dll")):
                 # NOTE: would it change behavior to load ALL
                 # DLLs at this path vs. the name restriction?
                 WinDLL(os.path.abspath(filename))
