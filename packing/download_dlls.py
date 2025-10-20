55204d37b7c935436f7f0eb39c9e74ee4e96ac98
Naveen M K <naveen521kk@gmail.com>
2025-10-20 11:43:19 +0000

    Bump PANGO_VERSION to 1.56.4-v1 in download scripts

--- packing/download_dlls.py
+++ packing/download_dlls.py
@@ -10,7 +10,7 @@ import zipfile
 from pathlib import Path
 from urllib.request import urlretrieve as download
 
-PANGO_VERSION = "1.56.4"
+PANGO_VERSION = "1.56.4-v1"
 
 
 def get_platform():
--- packing/download_pango_macos.py
+++ packing/download_pango_macos.py
@@ -10,7 +10,7 @@ import zipfile
 from pathlib import Path
 from urllib.request import urlretrieve as download
 
-PANGO_VERSION = "1.56.4"
+PANGO_VERSION = "1.56.4-v1"
 
 logging.basicConfig(format="%(levelname)s - %(message)s", level=logging.DEBUG)
 
